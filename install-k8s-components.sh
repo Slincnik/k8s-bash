90e0bd384490ac67e78ceb814d53b1531d7e3b59
Slincnik <pattyshow21@mail.ru>
2025-04-15 20:13:34 +0300

    chore: updating script

--- install-k8s-components.sh
+++ install-k8s-components.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-set -e # exit on first error
+set -euo pipefail # exit on first error
 
-# --- Check arguments ---
+# --- Check if running as root ---
+if [[ "$EUID" -ne 0 ]]; then
+  echo "Run this script as root or use sudo."
+  exit 1
+fi
+
+# --- Parameters ---
+
 USER_CONFIG=""
+USER_CONFIG_DEFAULT="/etc/containerd/config.toml"
+CNI_VERSION="v1.6.2"
+K8S_VERSION="v1.32"
+K8S_URL="https://pkgs.k8s.io/core:/stable:/${K8S_VERSION}/deb"
+
+# --- Parse arguments ---
 while [[ "$#" -gt 0 ]]; do
-  case $1 in
+  case "$1" in
     --config) USER_CONFIG="$2"; shift ;;
-    *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    --help|-h)
+      echo "Usage: $0 [--config <path_to_config.toml>]"
+      exit 0
+      ;;
+    *) echo "Unknown parameter passeds: $1"; exit 1 ;;
   esac
   shift
 done
@@ -21,14 +38,13 @@ if [[ -n "$USER_CONFIG" ]]; then
 fi
 
 
-
 echo "Install basic dependencies"
-sudo apt-get update & apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo apt-get update && apt-get install -y apt-transport-https ca-certificates curl gpg
 
 echo "Setting up core and system parameters"
 
 echo "Adding settings to sysctl.conf"
-cat <<EOF | sudo tee /etc/sysctl.conf
+tee /etc/sysctl.d/99-custom.conf > /dev/null <<EOF
 net.core.somaxconn = 1024
 net.ipv4.tcp_max_syn_backlog = 2048
 net.ipv4.ip_local_port_range = 10240 65535
@@ -43,22 +59,20 @@ EOF
 
 echo "Create k8s config"
 sudo touch /etc/sysctl.d/kubernetes.conf
-sudo bash -c 'cat <<EOF > /etc/sysctl.d/kubernetes.conf
+tee /etc/sysctl.d/kubernetes.conf > /dev/null <<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
-EOF'
+EOF
 
 echo "Loading kernel modules"
-sudo modprobe overlay 
-sudo modprobe br_netfilter
-
-echo "Enabling kernel modules"
+modprobe overlay || echo "Overlay module doen't exist"
+modprobe br_netfilter || echo "br_netfilter module doesn't exist"
 echo "overlay" | sudo tee -a /etc/modules
 echo "br_netfilter" | sudo tee -a /etc/modules
 
 echo "Enabling ip_forwarding"
-echo "1" | sudo tee -a /proc/sys/net/ipv4/ip_forward
+echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward
 
 echo "Reload sysctl"
 sudo systemctl daemon-reexec
@@ -83,20 +97,21 @@ sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/c
 
 echo "Install CNI plugins"
 sudo mkdir -p /opt/cni/bin
-wget -q https://github.com/containernetworking/plugins/releases/download/v1.6.2/cni-plugins-linux-amd64-v1.6.2.tgz -O /tmp/cni-plugins.tgz
+curl -sSL "https://github.com/containernetworking/plugins/releases/download/${CNI_VERSION}/cni-plugins-linux-amd64-${CNI_VERSION}.tgz" -o /tmp/cni-plugins.tgz
 sudo tar -xzf /tmp/cni-plugins.tgz -C /opt/cni/bin
 sudo chmod -R 0755 /opt/cni/bin
 rm /tmp/cni-plugins.tgz
 
 echo "Disable swapping"
 sudo swapoff -a
+sed -i.bak '/ swap / s/^/#/' /etc/fstab
 
 echo "Restart containerd"
 sudo systemctl restart containerd
 
 echo "Install kubeadm, kubelet and kubectl"
-sudo curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo curl -fsSL "${K8S_URL}/Release.key" | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] ${K8S_URL}/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 
 sudo apt-get install -y kubelet kubeadm kubectl
